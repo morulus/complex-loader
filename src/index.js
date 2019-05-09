9e00edfe61f0269df5c49b0f76c162a7b9ad406b
Morulus <vladimirmorulus@gmail.com>
2019-05-09 18:54:12 +0300

    The builder migrates to the @babel

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const loaderUtils = require(`loader-utils`);
 const mapValues = require(`lodash.mapvalues`);
 const map = require(`lodash.map`);
 const memoize = require(`lodash.memoize`);
-const babel = require(`babel-core`);
 
 const normalizeLoader = require(`./normalizeLoader`);
 const querifyLoader = require(`./querifyLoader`);
@@ -26,25 +25,6 @@ const shapeToQueries = memoize(function (shape) {
       );
 });
 
-/* Babel repl */
-const repl = (code) => babel.transform(code, {
-  presets: [
-    [
-require.resolve(`babel-preset-env`), {
-      targets: {
-        ie: 10
-      }
-    }
-]
-  ],
-  ast: false,
-  babelrc: false,
-  comments: false,
-  compact: true,
-  filename: `md.chunk.js`,
-  sourceType: `module`
-});
-
 module.exports = function () {};
 
 module.exports.pitch = function richLoader() {
@@ -56,12 +36,6 @@ module.exports.pitch = function richLoader() {
       queries,
       (query, key) => `module.exports[${JSON.stringify(key)}]=require('!${query}!${this.resourcePath}');`
     );
-
-    /* Const result = repl(`
-       module.exports = {
-       ${output.join(`,\n`)}
-       }
-       `).code; */
 
     return `module.exports = {};${output.join(`\n`)}`;
   } catch (e) {
